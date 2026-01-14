5405770770adafb813500a22f05430cc97038f1d
raymond <waymondrang@gmail.com>
2026-01-14 10:35:53 -0800

    add copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: "production",
@@ -22,4 +23,18 @@ module.exports = {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "test"),
     },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: "src/popup.html",
+                    to: "popup.html",
+                },
+                {
+                    from: "src/popup.css",
+                    to: "popup.css",
+                },
+            ],
+        }),
+    ],
 };
