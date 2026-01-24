f9826f0c669ae5d2b969533652a3819e49e9826e
raymond <waymondrang@gmail.com>
2026-01-24 11:24:48 -0800

    update eslint config, add unused private class members rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,16 @@ import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
+    {
+        ignores: ["webpack.config.cjs"],
+    },
     eslint.configs.recommended,
     tseslint.configs.recommended,
+    {
+        rules: {
+            // Note: you must disable the base rule as it can report incorrect errors
+            "no-unused-private-class-members": "off",
+            "@typescript-eslint/no-unused-private-class-members": "error",
+        },
+    },
 ]);
