1eeab5fcc5d931b1a930ef417a96c29ca46da0fe
raymond <waymondrang@gmail.com>
2026-02-10 15:09:03 -1000

    update webpack build command and config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -4,63 +4,71 @@ const webpack = require("webpack");
 const { execSync } = require("child_process");
 const fs = require("fs");
 
-module.exports = (env, argv) => {
-    let commitHash = execSync("git rev-parse HEAD", {
-        encoding: "utf8",
-    }).trim();
+let commitHash = execSync("git rev-parse HEAD", {
+    encoding: "utf8",
+}).trim();
 
-    const packageJSON = JSON.parse(fs.readFileSync("package.json", "utf8"));
+const packageJSON = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
-    return {
-        mode: "production",
-        entry: {
-            docs: "./src/docs.ts",
-            popup: "./src/popup.ts",
-        },
-        module: {
-            rules: [
+const config = {
+    entry: {
+        docs: "./src/docs.ts",
+        popup: "./src/popup.ts",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "build"),
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
                 {
-                    test: /\.tsx?$/,
-                    use: "ts-loader",
-                    exclude: /node_modules/,
+                    from: "src/popup.html",
+                    to: "popup.html",
+                },
+                {
+                    from: "src/manifest.json",
+                    to: "manifest.json",
+                    transform(content, path) {
+                        const manifest = JSON.parse(content.toString());
+                        manifest.version = packageJSON.version;
+                        return JSON.stringify(manifest, null, 2);
+                    },
+                },
+                {
+                    from: "src/assets",
+                    to: "assets",
                 },
             ],
-        },
-        resolve: {
-            extensions: [".tsx", ".ts", ".js"],
-        },
-        output: {
-            filename: "[name].bundle.js",
-            path: path.resolve(__dirname, "build"),
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                // See: https://webpack.js.org/configuration/mode/
-                __IS_PRODUCTION__: JSON.stringify(argv.mode === "production"),
-                __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
-                __BUILD_COMMIT__: JSON.stringify(commitHash),
-            }),
-            new CopyWebpackPlugin({
-                patterns: [
-                    {
-                        from: "src/popup.html",
-                        to: "popup.html",
-                    },
-                    {
-                        from: "src/manifest.json",
-                        to: "manifest.json",
-                        transform(content, path) {
-                            const manifest = JSON.parse(content.toString());
-                            manifest.version = packageJSON.version;
-                            return JSON.stringify(manifest, null, 2);
-                        },
-                    },
-                    {
-                        from: "src/assets",
-                        to: "assets",
-                    },
-                ],
-            }),
-        ],
-    };
+        }),
+    ],
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === "development") {
+        config.devtool = "source-map";
+    }
+
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            // See: https://webpack.js.org/configuration/mode/
+            __IS_PRODUCTION__: JSON.stringify(argv.mode === "production"),
+            __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
+            __BUILD_COMMIT__: JSON.stringify(commitHash),
+        })
+    );
+
+    return config;
 };
